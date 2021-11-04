1b458e04bee002eceb8b6d7064bb72bdee2f21f6
dalgonaio <8888888dalgonaio@users.noreply.github.com>
2021-11-03 22:04:43 -0400

    style: updated main page to style header

--- screens/Home.js
+++ screens/Home.js
@@ -4,8 +4,11 @@ import HeaderTabs from '../components/HeaderTabs'
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{backgroundColor: "#eee", flex:1}}>
+      <View style={{backgroundColor: "white", padding:15,}}>
       <HeaderTabs />
+      </View>
+
     </SafeAreaView>
   )
 }
