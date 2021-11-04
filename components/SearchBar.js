5948edb60b2b08391f10dd334af14a2810a22ed8
dalgonaio <jungmeevlee@gmail.com>
2021-11-04 19:12:15 -0400

    Component: Search bar added on front end and installed google places autocomplete

---
+++ components/SearchBar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import Ionicons from "react-native-vector-icons/Ionicons"
+import AntDesign from "react-native-vector-icons/AntDesign"
+
+const SearchBar = () => {
+  return (
+    <View style= {styles.container}>
+      <GooglePlacesAutocomplete
+      placeholder = "Search"
+      styles = {{
+        textInput:{
+          backgroundColor: "#eee",
+          borderRadius: 20,
+          fontWeight: "700",
+          marginTop: 7,
+        },
+        textInputContainer:{
+          backgroundColor:"#eee",
+          borderRadius: 50,
+          flexDirection: "row",
+          alignItems: "center",
+          marginRight: 10,
+        },
+      }}
+      renderLeftButton = {()=> (
+        <View style= {{marginLeft: 10}}>
+          <Ionicons
+          name= "location-sharp"
+          size={24}
+          />
+          </View>
+      )}
+
+      renderRightButton= {()=>(
+        <View style={{
+          marginRight:10,
+          flexDirection:"row",
+          backgroundColor: "white",
+          borderRadius: 20,
+          padding:10,
+          alignItems: "center"
+        }}>
+          <AntDesign
+          name="clockcircle"
+          size={11}
+          style={{
+            marginRight:6
+          }} />
+        <Text>Search</Text>
+        </View>
+      )}
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    flexDirection: "row"
+  }
+})
