53a74640d660006a3f2feaf036b6119babac4b9f
dalgonaio <jungmeevlee@gmail.com>
2021-11-05 21:43:58 -0400

    Component: categories component rendered

--- components/Categories.js
+++ components/Categories.js
@@ -1,42 +1,87 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View, Image, ScrollView, ImageStore} from 'react-native';
 
-//need to add additional categories and change fast food png
+//change coffee and tea image
+
 const items = [
   {
     image: require('../assets/images/ShoppingBag2.png'),
-    text: 'Pick up'
+    text: 'Pick up',
   },
   {
     image: require('../assets/images/drinks.png'),
-    text: 'Soft Drinks'
+    text: 'Soft Drinks',
   },
   {
     image: require('../assets/images/bakery.png'),
-    text: 'Bakery'
+    text: 'Bakery',
+  },
+  {
+    image: require('../assets/images/fastfood.png'),
+    text: 'Fast Food',
+  },
+  {
+    image: require('../assets/images/deals.png'),
+    text: 'Deals',
   },
   {
     image: require('../assets/images/bakery.png'),
-    text: 'Fast Food'
+    text: 'Coffe & Tea',
   },
-  {
-    image: require('../assets/images/bakery.png'),
-    text: 'Deals'
-  },
-  {
-    image: require('../assets/images/bakery.png'),
-    text: 'Coffe & Tea'
-  }
 ];
 
 const Categories = () => {
   return (
-    <View>
-      <Text>hi</Text>
+    <View
+    style={{
+      marginTop:5,
+      backgroundColor:"white",
+      paddingVertical:10,
+      paddingLeft: 20,
+    }}>
+      <ScrollView
+    horizontal
+    showsHorizontalScrollIndicator={false}
+    // contentContainerStyle = {styles.content}
+    >
+        {items.map((item, idx) => {
+          return (
+            <View style={{alignItems:"center", marginRight:30, marginLeft:30}}
+            key={idx}>
+              <Image
+                source={item.image}
+                style={{
+                  width: 50,
+                  height: 40,
+                  resizeMode: 'contain',
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 13,
+                  fontWeight: '900',
+                }}
+              >
+                {item.text}
+              </Text>
+              </View>
+
+          );
+        })}
+       </ScrollView>
     </View>
-  )
-}
 
-export default Categories
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Categories;
+
+const styles = StyleSheet.create({
+  // content:{
+    // flex:1,
+    // flexDirection: "row",
+    // justifyContent:"space-between",
+    // marginLeft:30,
+    // marginRight:30,
+  // }
+});
