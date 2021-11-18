903bc39e080607a736d1e082aca5c2b0d0fd5d09
dalgonaio <jungmeevlee@gmail.com>
2021-11-17 22:09:02 -0500

    Cleanup: added comments to code

--- screens/Home.js
+++ screens/Home.js
@@ -6,16 +6,15 @@ import HeaderTabs from '../components/HeaderTabs';
 import RestaurantItems, {localRestaurants} from '../components/RestaurantItem';
 import SearchBar from '../components/SearchBar';
 
+//Yelp Developer API Key
 const apikey ="B1iXCN9N8nmNaWtNVd6FWAWeTK-eP9sLRj4hLlxek8W1JwRTv8toIxAaJZ4Tq1v1LX5PYcwtQwnjPblLfGOmnYT4JJ-RFgLaJmcaDmJ4EslWHiolr_j3NaGmsb6VYXYx";
-
-
-
 
 
 
 const Home = () => {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
 
+  //call to Yelp for restaurant data
   const getRestaurantsFromYelp = () =>{
     const yelpURL = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=NewYorkCity"
 
@@ -29,6 +28,7 @@ const Home = () => {
       .then(json=>setRestaurantData(json.businesses))
     }
 
+    //set up state with data pulled from yelp upon loading
   useEffect(()=>{
     getRestaurantsFromYelp();
  }, [])
