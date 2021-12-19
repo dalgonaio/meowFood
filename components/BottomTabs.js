38e184200fda991a240903e70fc0ccd65997387c
dalgonaio <jungmeevlee@gmail.com>
2021-12-19 13:48:49 -0500

    Feat: Bottom Tabs component

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,13 +1,36 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { View, Text } from 'react-native'
 
-const BottomTabs = () => {
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+export default function BottomTabs() {
   return (
-    <View>
-      <Text>Bottom Tabs</Text>
+    <View style={styles.container}>
+      <Icon icon="home" text="Home" />
+      <Icon icon="search" text="Browse" />
+      <Icon icon="shopping-bag" text="Groceries" />
+      <Icon icon="receipt" text="Orders" />
+      <Icon icon="user" text="Account" />
     </View>
-  )
+  );
 }
 
-export default BottomTabs
+const Icon = (props) => (
+  <View>
+    <FontAwesome5 name={props.icon}
+    size={25}
+    style={{marginBottom: 3, alignSelf: 'center'}}
+    />
+    <Text>{props.text}</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    marginHorizontal: 30,
+  },
+});
