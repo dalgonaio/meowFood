71fbe2bf61a7464af1fe63131cb3f1a3150fbb3d
dalgonaio <jungmeevlee@gmail.com>
2021-12-27 12:01:33 -0500

    Patch: update react native version"

---
+++ Navigation.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Navigation = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Navigation
